652ff3efccbee2e8615376962dab79d42dc50cd6
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-07-08 13:14:13 +0200

    Initial commit

---
+++ CI/scripts/modify_magento.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -xe
+
+NGROK_HOST=${1}
+
+sleep 10
+sudo docker exec --interactive magento /bin/bash <<EOF
+php bin/magento setup:store-config:set --base-url="http://${NGROK_HOST}/"
+php bin/magento setup:store-config:set --base-url-secure="https://${NGROK_HOST}/"
+php bin/magento cache:flush
+EOF
---
+++ CI/scripts/ngrok.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -xe
+
+function get_ngrok_url() {
+  curl --fail -s localhost:4040/api/tunnels | jq -r .tunnels\[0\].public_url | sed 's/^http:/https:/'
+}
+
+function wait_for_ngrok() {
+  while [[ -z ${RESPONSE} || ${RESPONSE} == 'null' ]]; do
+    RESPONSE=$(get_ngrok_url)
+    sleep 3;
+  done
+}
+
+NGROK_TOKEN=${1}
+
+if [[ -z ${NGROK_TOKEN} ]]; then
+  echo 'NGROK token missing. Set NGROK_TOKEN env' >&2
+  exit 1
+fi
+
+./node_modules/ngrok/bin/ngrok authtoken ${NGROK_TOKEN}
+./node_modules/ngrok/bin/ngrok http 80 >&/dev/null &
+wait_for_ngrok
+export NGROK_URL=$(get_ngrok_url)
+echo ${NGROK_URL}
