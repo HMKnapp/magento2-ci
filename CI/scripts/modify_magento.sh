d139aa59503cc4915130958b61edefc43577452e
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-08-17 11:08:31 +0200

    Change composer call

--- CI/scripts/modify_magento.sh
+++ CI/scripts/modify_magento.sh
@@ -4,7 +4,7 @@ set -e
 
 NGROK_HOST=${1}
 
->&2 echo -n "Waiting for shop "
+>&2 echo "Waiting for shop to start up.. "
 while [[ -z $(curl -Lks http://localhost/ | grep '2013-present Magento') ]]; do 
   sleep 1
   >&2 echo -n .
@@ -12,8 +12,8 @@ done
 >&2 echo
 sudo docker exec --interactive magento /bin/bash <<EOF
 cd /opt/bitnami/magento
-echo "Setting Magento Adobe auth tokens"
-composer config --global --auth http-basic.repo.magento.com ${MAGENTO_ACC_PUBKEY} ${MAGENTO_ACC_PRIVKEY}
+echo "Setting Magento Adobe Auth token for ${MAGENTO_ACC_PUBKEY}"
+composer config http-basic.repo.magento.com ${MAGENTO_ACC_PUBKEY} ${MAGENTO_ACC_PRIVKEY}
 echo "Changing magento base-url to https://${NGROK_HOST}/"
 php bin/magento setup:store-config:set --base-url="http://${NGROK_HOST}/"
 php bin/magento setup:store-config:set --base-url-secure="https://${NGROK_HOST}/"
