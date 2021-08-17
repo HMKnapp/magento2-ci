c9ad66ca7c0a33722eca7b48e1c8edc47d0bbe8d
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-08-17 11:00:43 +0200

    Authenticate composer to Magento repo

--- CI/scripts/modify_magento.sh
+++ CI/scripts/modify_magento.sh
@@ -12,6 +12,8 @@ done
 >&2 echo
 sudo docker exec --interactive magento /bin/bash <<EOF
 cd /opt/bitnami/magento
+echo "Setting Magento Adobe auth tokens"
+composer config --global --auth http-basic.repo.magento.com ${MAGENTO_ACC_PUBKEY} ${MAGENTO_ACC_PRIVKEY}
 echo "Changing magento base-url to https://${NGROK_HOST}/"
 php bin/magento setup:store-config:set --base-url="http://${NGROK_HOST}/"
 php bin/magento setup:store-config:set --base-url-secure="https://${NGROK_HOST}/"
