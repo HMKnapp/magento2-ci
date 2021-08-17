f457ff0d8fb01087da1f5d3d82f9d212405fb84d
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-08-17 11:25:41 +0200

    Remove magento:upgrade step

--- CI/scripts/modify_magento.sh
+++ CI/scripts/modify_magento.sh
@@ -14,12 +14,11 @@ sudo docker exec --interactive magento /bin/bash <<EOF
 cd /opt/bitnami/magento
 echo "Setting Magento Adobe Auth token for ${MAGENTO_ACC_PUBKEY}"
 composer config http-basic.repo.magento.com ${MAGENTO_ACC_PUBKEY} ${MAGENTO_ACC_PRIVKEY}
+echo "Installing sample data"
+php bin/magento sampledata:deploy
 echo "Changing magento base-url to https://${NGROK_HOST}/"
 php bin/magento setup:store-config:set --base-url="http://${NGROK_HOST}/"
 php bin/magento setup:store-config:set --base-url-secure="https://${NGROK_HOST}/"
-echo "Flushing cache"
+echo "Flushing cache.."
 php bin/magento cache:flush >&/dev/null
-echo "Installing sample data"
-php bin/magento sampledata:deploy
-php bin/magento setup:upgrade
 EOF
