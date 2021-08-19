f36680d8ccfd6af277bbfd09e82f4233c1b7baf9
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-08-19 09:53:04 +0200

    Increase memory limits because upgrade is unstable

--- CI/scripts/modify_magento.sh
+++ CI/scripts/modify_magento.sh
@@ -11,6 +11,8 @@ while [[ -z $(curl -Lks http://localhost/ | grep '2013-present Magento') ]]; do
 done
 >&2 echo
 sudo docker exec --interactive magento /bin/bash <<EOF
+echo "Set memory_limit in magento .htacces to 8G"
+sed -i 's/php_value\ memory_limit\ 756M/php_value memory_limit 8G/' /opt/bitnami/magento/pub/.htaccess
 cd /opt/bitnami/magento
 echo "Setting Magento Adobe Auth token for ${MAGENTO_ACC_PUBKEY}"
 composer config http-basic.repo.magento.com ${MAGENTO_ACC_PUBKEY} ${MAGENTO_ACC_PRIVKEY}
@@ -20,7 +22,6 @@ php bin/magento setup:store-config:set --base-url-secure="https://${NGROK_HOST}/
 echo "Flushing cache"
 php bin/magento cache:flush >&/dev/null
 echo "Installing sample data"
-php bin/magento sampledata:deploy
-php bin/magento setup:upgrade
-php bin/magento setup:di:compile
-EOF
+php -dmemory_limit=8G bin/magento sampledata:deploy
+php -dmemory_limit=8G bin/magento setup:upgrade
+php -dmemory_limit=8G bin/magento setup:di:compileEOF
