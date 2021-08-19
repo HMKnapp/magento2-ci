e84f3c5038b6add3d30a7f568ba4a567f3102444
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-08-19 10:08:48 +0200

    Install unzip for composer file permissions

--- CI/scripts/modify_magento.sh
+++ CI/scripts/modify_magento.sh
@@ -11,6 +11,8 @@ while [[ -z $(curl -Lks http://localhost/ | grep '2013-present Magento') ]]; do
 done
 >&2 echo
 sudo docker exec --interactive magento /bin/bash <<EOF
+echo "Install unzip for composer file permissions"
+apt-get update && apt-get -y install unzip
 echo "Set memory_limit in magento .htacces to 8G"
 sed -i 's/php_value\ memory_limit\ 756M/php_value memory_limit 8G/' /opt/bitnami/magento/pub/.htaccess
 cd /opt/bitnami/magento
@@ -24,4 +26,5 @@ php bin/magento cache:flush >&/dev/null
 echo "Installing sample data"
 php -dmemory_limit=8G bin/magento sampledata:deploy
 php -dmemory_limit=8G bin/magento setup:upgrade
-php -dmemory_limit=8G bin/magento setup:di:compileEOF
+php -dmemory_limit=8G bin/magento setup:di:compile
+EOF
