e9c96ab11339d1d35a9fb8bb68104b9357cf17f1
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-09-17 11:55:17 +0200

    Tr without unzip, htaccess modifications

--- CI/scripts/modify_magento.sh
+++ CI/scripts/modify_magento.sh
@@ -11,10 +11,10 @@ while [[ -z $(curl -Lks http://localhost/ | grep '2013-present Magento') ]]; do
 done
 >&2 echo
 sudo docker exec --interactive magento /bin/bash <<EOF
-echo "Install unzip for composer file permissions"
-apt-get update && apt-get -y install unzip
-echo "Set memory_limit in magento .htacces to 8G"
-sed -i 's/php_value\ memory_limit\ 756M/php_value memory_limit 8G/' /opt/bitnami/magento/pub/.htaccess
+# echo "Install unzip for composer file permissions"
+# apt-get update && apt-get -y install unzip
+# echo "Set memory_limit in magento .htacces to 8G"
+# sed -i 's/php_value\ memory_limit\ 756M/php_value memory_limit 8G/' /opt/bitnami/magento/pub/.htaccess
 cd /opt/bitnami/magento
 echo "Setting Magento Adobe Auth token for ${MAGENTO_ACC_PUBKEY}"
 composer config http-basic.repo.magento.com ${MAGENTO_ACC_PUBKEY} ${MAGENTO_ACC_PRIVKEY}
